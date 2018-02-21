78cd83fad491c92a855a96d2b50570da13c69766
Mirza Sisic <mirzasisic57@gmail.com>
2018-02-21 12:03:16 -0500

    Test

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//One line comment 
+/*
+ Multi
+ line
+ comment
+     */
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}  
