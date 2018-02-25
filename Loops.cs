613d055a7c99c2c2b045e94fb6c3bd21cbf90db9
Mirza Sisic <mirzasisic57@gmail.com>
2018-02-25 15:01:02 -0500

    Loops

--- Loops.cs
+++ Loops.cs
@@ -12,7 +12,12 @@ namespace Iteration
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                //Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found 7!");
+                    break;
+                }
             }
             Console.ReadLine();
         }
