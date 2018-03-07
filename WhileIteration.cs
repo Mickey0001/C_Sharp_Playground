40ef065d8508544ebe2b104a1b4a8c4b61b7031f
Mirza Sisic <mirzasisic57@gmail.com>
2018-03-07 12:17:05 -0500

    While Iteration

---
+++ WhileIteration.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+      
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("(1) Print Numbers ");
+            Console.WriteLine("(2) GuessingGame");
+            Console.WriteLine("(3) Exit");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                PrintNumbers();
+                return true;
+            }
+            else if (result == "2")
+            {
+                GuessingGame();
+                return true;
+            }
+            else if (result == "3")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.Clear();
+            Console.WriteLine("Print Numbers");
+            Console.WriteLine("Type a number: ");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter < result + 1)
+            {
+                Console.Write(counter);
+                Console.Write("-");
+                counter++;
+            }
+            Console.ReadLine();
+        }
+
+        private static void GuessingGame()
+        {
+            Console.Clear();
+            Console.WriteLine("GuessingGame");
+
+
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 11);
+
+            int guuesses = 0;
+            bool incorerct = true;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+                string result = Console.ReadLine();
+                guuesses++;
+
+                if (result == randomNumber.ToString())
+                    incorerct = false;
+                else
+                    Console.WriteLine("Wrong!");
+
+
+
+            } while (incorerct);
+            Console.WriteLine("Correct! It Toook you {0} guuesses.", guuesses);
+
+            Console.ReadLine();
+        }
+    }
+}
