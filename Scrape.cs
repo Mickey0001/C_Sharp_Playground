00487b95da656a86d28424401348212e3e673280
Mirza Sisic <mirzasisic57@gmail.com>
2018-03-14 14:04:06 -0400

    Scrape library

---
+++ Scrape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ScrapeClient
+{
+    public class Scrape
+    {
+           public string ScrapeWebpage(string url)
+    {
+        return GetWebpage(url);
+    }
+
+    public string ScrapeWebpage(string url, string filepath)
+    {
+        string reply = GetWebpage(url);
+
+        File.WriteAllText(filepath, reply);
+        return reply;
+    }
+
+    private string GetWebpage(string url)
+    {
+        WebClient client = new WebClient();
+        return client.DownloadString(url);
+    }
+    }
+}
+ 
+
