fdde7929593f139d0983b0e2fee9420b20c52d35
Mirza Sisic <mirzasisic57@gmail.com>
2018-09-19 19:23:24 +0200

    Album Model

---
+++ MVC_MusicStore/MVC_MusicStore/Models/Album.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string Title { get; set; }
+
+        public Artist Artist { get; set; }
+    }
+}
