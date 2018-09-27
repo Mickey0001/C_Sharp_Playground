a86ad7ef83621aaf486d3b7085b3b83281f4a581
Mirza Sisic <mirzasisic57@gmail.com>
2018-09-27 16:56:23 +0200

    Actions

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -14,11 +14,16 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            //return View(movie);
+            return View(movie);
             //return Content("Hello!");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
         }
     }
 }
