950557b396252a7574adcfb9c642b453b2bbc589
Mirza Sisic <mirzasisic57@gmail.com>
2018-09-28 02:24:54 +0200

    Vidly Updates

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -9,33 +9,8 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
+        public ActionResult Random { get; set; }
 
-            return View(movie);
-            //return Content("Hello!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id= " + id);
-        }
-
-        //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        public ActionResult ByReleaseDate(int year, int month) => Content(year + "/" + month);
     }
 }
