fc1ae776a924a1ebc81bd110a935232cf8d0ccce
Mirza Sisic <mirzasisic57@gmail.com>
2018-09-29 15:13:39 +0200

    Vidly Updates

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
+    [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
     public class MoviesController : Controller
     {
-        public ActionResult Random { get; set; }
+       //Get:Movies/Random
+       public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
 
-        public ActionResult ByReleaseDate(int year, int month) => Content(year + "/" + month);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+           
+
+            return View(viewModel);
+        }
     }
 }
