cce3aa1fb4fb49790efa2b58271fb7a83b59a997
Mickey0001 <mirzasisic57@gmail.com>
2019-01-16 19:52:38 +0100

    C# primitives

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -6,7 +6,19 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var name = "Mirzi";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(name);
+            Console.WriteLine(isWorking);
         }
     }
 }
