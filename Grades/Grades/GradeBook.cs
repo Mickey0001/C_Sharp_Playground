8a32e66baf57cfc939cce30ec7101f74f6cb5c97
Mickey0001 <mirzasisic57@gmail.com>
2019-02-23 18:53:46 +0100

    Practicing C#

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades = new List<float>();
+    }
+}
