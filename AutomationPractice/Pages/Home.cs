6c2cc6a76a30819f8c07e7c7eb7e95bb334bf4dc
Mickey0001 <mirzasisic57@gmail.com>
2019-07-11 23:07:16 +0200

    Add home class

---
+++ AutomationPractice/Pages/Home.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumProject
+{
+    public class Home
+    {
+        //Locators
+        [FindsBy(How = How.CssSelector, Using = "#small-searchterms")]
+        private IWebElement SearchStoreInput;
+
+        [FindsBy(How = How.XPath, Using = "//input[@value='Search']")]
+        private IWebElement SearchButton;
+
+        //Actions
+        public void isAt()
+        {
+            Assert.IsTrue(Browsers.Title.Equals("nopCommerce demo store"));
+        }
+        public void EnterSearchText(string searchText)
+        {
+            Assert.IsTrue(SearchStoreInput.Displayed);
+            SearchStoreInput.SendKeys(searchText);
+        }
+    }
+}
