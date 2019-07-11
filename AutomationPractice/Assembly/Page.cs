b21ff2f19647e8af88dcdaacda808467b668bad1
Mickey0001 <mirzasisic57@gmail.com>
2019-07-11 23:08:46 +0200

    Add Page class and browsers

---
+++ AutomationPractice/Assembly/Page.cs
@@ -0,0 +1,18 @@
+﻿using SeleniumExtras.PageObjects;
+
+namespace SeleniumProject
+{
+    public static class Pages
+    {
+        private static T getPages() where T : new()
+        {
+            var page = new T();
+            PageFactory.InitElements(Browsers.getDriver, page);
+            return page;
+        }
+        public static Home home
+        {
+            get { return getPages(); }
+        }
+    }
+}
