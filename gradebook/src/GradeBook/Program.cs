8a50278ec1a39d588dda0538b8b998a255e545b6
MirzaSisic <mirzasisic57@gmail.com>
2019-07-17 17:57:47 +0200

    Mirza is the best

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,7 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mirza is the best");
         }
     }
 }
