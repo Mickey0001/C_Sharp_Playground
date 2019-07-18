2db82127e4dbfbdc48a6cf2dad8eab2b330ed76f
MirzaSisic <mirzasisic57@gmail.com>
2019-07-18 23:33:08 +0200

    Variables and types

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,6 +6,13 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+           double x = 34.1;
+
+            var a = 1;
+            int b = 4;
+            IAsyncResult = a + b;
+            Console.WriteLine(result);
+
             if(args.Length > 0)
             {
                Console.WriteLine($"{args[0]} is the best ");
