d9af34232d31705dafcfcd4ddbc23e89e41a2cfd
MirzaSisic <mirzasisic57@gmail.com>
2019-07-22 22:39:02 +0200

    Working on classes

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+            book.AddGrade(88.1);
+
             var grades = new List<double>() { 11.3, 35.2, 19.5 };
             grades.Add(15.5);
 
@@ -18,15 +22,6 @@ namespace GradeBook
 
             result /= grades.Count;
             Console.WriteLine($"The average grade is {result:N1}");
-
-            if(args.Length > 0)
-            {
-               Console.WriteLine($"{args[0]} is the best ");
-            }
-            else 
-            {
-                Console.WriteLine("Meh");
-            }
         }
     }
 }
