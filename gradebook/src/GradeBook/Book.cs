fb389c2c8b4265e4bd4d717a9c335e5a1bec0a2a
MirzaSisic <mirzasisic57@gmail.com>
2019-07-23 22:08:23 +0200

    Constructor

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,10 +1,18 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
         public void AddGrade(double grade)
         {
-            
+            grades.Add(grade);
         }
+        List<double> grades;
     }
 }
