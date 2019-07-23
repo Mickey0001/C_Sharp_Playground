f882874d2a60d8bb9ac2f981d346f171183e5660
MirzaSisic <mirzasisic57@gmail.com>
2019-07-23 23:10:34 +0200

    Working on Encapsulation

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -12,9 +13,28 @@ namespace GradeBook
 
         public void AddGrade(double grade)
         {
-            this.grades.Add(grade);
+            grades.Add(grade);
         }
-        private List<double> grades;
+
+
+        public void ShowStatistics()
+        {   
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach(var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
         private string name;
+        private List<double> grades;
+        }
     }
-}
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -11,19 +11,8 @@ namespace GradeBook
             var book = new Book("Mirzaki Gradebookie");
             book.AddGrade(88.1);
             book.AddGrade(90.5);
-            book.grades.Add(101);
-
-            var grades = new List<double>() { 11.3, 35.2, 19.5 };
-            grades.Add(15.5);
-
-            var result = 0.0;
-            foreach(var number in grades)
-            {
-                result += number;
-            }
-
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
+            book.AddGrade(75.1);
+            book.ShowStatistics();
         }
     }
 }
