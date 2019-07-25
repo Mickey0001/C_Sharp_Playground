793b36d8ef6d814974f88da38230c3d8f68eb16b
MirzaSisic <mirzasisic57@gmail.com>
2019-07-25 22:59:43 +0200

    Value and Ref Assertions

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -15,11 +15,25 @@ namespace GradeBook.Tests
         {
             var book1 = GetBook("Book1");
             var book2 = GetBook("Book2");
+
+            Assert.AreEqual("Book1", book1.Name);
+            Assert.AreEqual("Book2", book2.Name);
+            Assert.AreNotSame(book1, book2);
         }
 
         private Book GetBook(string name)
         {
             return new Book(name);
+        }
+
+        [Test]
+        public void TwoVaribablesCanRefrenceTheSameType()
+        {
+            var book1 = GetBook("Book1");
+            var book2 = book1;
+
+            Assert.AreSame(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
         }
     }
 }
