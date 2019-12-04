ea35cbe810e8a1138efe25f0fe02cb9db75eae8a
MirzaSisic <mirzasisic57@gmail.com>
2019-12-04 22:08:51 +0100

    ProductClass

---
+++ CustomerManagment/CustomerManagmentBusinessLayer/Product.cs
@@ -0,0 +1,42 @@
+﻿namespace CustomerManagmentBusinessLayer
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productID)
+        {
+            ProductID = productID;
+        }
+
+        public decimal? CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductID { get; private set; }
+        public string ProductName { get; set; }
+
+        public Product Retrieve(int productID)
+        {
+            return new Product();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+        }
+    }
+
+
+}
