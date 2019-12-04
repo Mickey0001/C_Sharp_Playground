c1091528b5db71dd8f8c5821b7b5f0b5e06178ae
MirzaSisic <mirzasisic57@gmail.com>
2019-12-04 22:14:58 +0100

    OrderClass

---
+++ CustomerManagment/CustomerManagmentBusinessLayer/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CustomerManagmentBusinessLayer
+{
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderID)
+        {
+            OrderID = orderID;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderID { get; set; }
+
+        public Order Retrieve(int orderID)
+        {
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
