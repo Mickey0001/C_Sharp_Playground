45268b62affbc0e157078ce040af8071f38a35e7
MirzaSisic <mirzasisic57@gmail.com>
2019-12-06 16:58:17 +0100

    CustomerRepositoryClass

---
+++ CustomerManagment/CustomerManagmentBusinessLayer/CustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace CustomerManagmentBusinessLayer
+{
+    public class CustomerRepository
+    {
+        public Customer Retrieve(int customerID)
+        {
+            //Create the instance of the Customer class
+            //Pass in the requested ID
+            Customer customer = new Customer(customerID);
+
+            //Code for retrieving the defined customer
+
+            //Temporary hard-coded values to return
+            //Example of a populated customer
+            if (customerID == 1)
+            {
+                customer.EmailAdrress = "bobosmrad@redtube.com";
+                customer.FirstName = "Bobo";
+                customer.LastName = "Smard";
+            }
+            return customer;
+
+        }
+        public bool Save(Customer customer)
+        {
+            //Saves the passed in customer
+            return true;
+        }
+    }
+}
