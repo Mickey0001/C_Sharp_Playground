461d2cde8524fadab254df1773796196bc0aa5b0
MirzaSisic <mirzasisic57@gmail.com>
2019-12-07 16:28:38 +0100

    ProductRepositoryClass

---
+++ CustomerManagment/CustomerManagmentBusinessLayer/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomerManagmentBusinessLayer
+{
+    public class ProductRepository
+    {
+        public Product Retrieve(int productID)
+        {
+            Product product = new Product(productID);
+
+            if (productID == 1)
+            {
+
+            }
+            return product;
+        }
+        public bool Save(Product product)
+        {
+            return true;
+        }
+     
+    }
+}
