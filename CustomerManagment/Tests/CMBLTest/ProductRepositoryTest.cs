54f09f7fc1c9f09e04f8ad1d952efbf194ac29e4
MirzaSisic <mirzasisic57@gmail.com>
2019-12-08 13:36:24 +0100

    ProductRepositoryTest

--- CustomerManagment/Tests/CMBLTest/ProductRepositoryTest.cs
+++ CustomerManagment/Tests/CMBLTest/ProductRepositoryTest.cs
@@ -10,7 +10,17 @@ namespace CMBLTest
         public void RetrieveTest()
         {
             var productRepository = new ProductRepository();
-            var expected = new Product(2);
+            var expected = new Product(2)
+            {
+                ProductName = "Lightsaber",
+                ProductDescription = "An elegant weapon for a more civilized age...",
+                CurrentPrice = 66
+            };
+            var actual = productRepository.Retrieve(2);
+
+            Assert.AreEqual(expected.CurrentPrice, actual.CurrentPrice);
+            Assert.AreEqual(expected.ProductDescription, actual.ProductDescription);
+            Assert.AreEqual(expected.ProductName, actual.ProductName);
         }
     }
 }
