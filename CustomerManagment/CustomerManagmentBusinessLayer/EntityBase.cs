1681b02b8006c456505591e9fbf52c14a7223df5
MirzaSisic <mirzasisic57@gmail.com>
2019-12-14 14:58:13 +0100

    BaseEntityClassToInheritFrom

---
+++ CustomerManagment/CustomerManagmentBusinessLayer/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomerManagmentBusinessLayer
+{
+    public enum EntityStateOption
+    {
+        Active,
+        Deleted
+    }
+
+    public abstract class EntityBase
+    {
+        public EntityStateOption Entity { get; set; }
+        public bool HasChanges { get; set; }
+        public bool IsNew { get; private set; }
+        public bool IsValid
+        {
+            get { return true;  }
+        }
+    }
+}
