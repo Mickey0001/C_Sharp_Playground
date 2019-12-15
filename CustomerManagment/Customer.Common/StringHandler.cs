1421d16d2e54520c36a8ca15d3de2e0c26d5a95a
MirzaSisic <mirzasisic57@gmail.com>
2019-12-15 15:05:03 +0100

    CommonLibraryForReusableCodeAddedStringHandlerClass

---
+++ CustomerManagment/Customer.Common/StringHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Customer.Common
+{
+    public class StringHandler
+    {
+     public string InsertSpaces(string source)
+        {
+            string result = string.Empty;
+
+            if (!String.IsNullOrWhiteSpace(source))
+            {
+                foreach (char letter in source)
+                {
+                    if (char.IsUpper(letter))
+                    {
+                        result += " ";
+                    }
+                    result += letter;
+                }
+            }
+            return result;
+        }
+    }
+}
