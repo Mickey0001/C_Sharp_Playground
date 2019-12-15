4536d4710620e02289891ce3f8ac04b36e09a30b
MirzaSisic <mirzasisic57@gmail.com>
2019-12-15 16:19:26 +0100

    ImpletemntTheExtendedMethodInProduct

--- CustomerManagment/CustomerManagmentBusinessLayer/Product.cs
+++ CustomerManagment/CustomerManagmentBusinessLayer/Product.cs
@@ -23,7 +23,7 @@ namespace CustomerManagmentBusinessLayer
         {
             get
             {
-                return StringHandler.InsertSpaces(_productName);
+                return _productName.InsertSpaces();
             }
             set
             {
