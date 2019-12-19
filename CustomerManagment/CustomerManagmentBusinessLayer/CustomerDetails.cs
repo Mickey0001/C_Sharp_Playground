6633d47772905590bb1526a31770e33b7602f4dd
MirzaSisic <mirzasisic57@gmail.com>
2019-12-19 23:38:20 +0100

    AddingUnitTestForLogging

---
+++ CustomerManagment/CustomerManagmentBusinessLayer/CustomerDetails.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Customer.Common;
+
+namespace CustomerManagmentBusinessLayer
+{
+    public class CustomerDetails : EntityBase, ILoggable
+    {
+        public CustomerDetails(): this(0)
+        {
+
+        }
+
+        public CustomerDetails(int customerID)
+        {
+            CustomerID = customerID;
+            AddressList = new List<Address>();
+        }
+
+        public List<Address> AddressList { get; set;  }
+        public int CustomerID { get; private set; }
+        public int CustomerType { get; set; }
+        public string EmailAdrress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public static int InstanceCount { get; set; }
+
+
+        public override string ToString() => FullName;
+
+        public override bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAdrress)) isValid = false;
+
+            return isValid;
+        }
+
+        public string Log() => $"{CustomerID}: {FullName} Email: {EmailAdrress} Status: {EntityState.ToString()}";
+
+    }
+}
---
+++ CustomerManagment/Tests/Customer.CommonTest/LogginServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Customer.Common;
+using CustomerManagmentBusinessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Customer.CommonTest
+{
+    [TestClass]
+    public class LogginServiceTest
+    {
+        [TestMethod]
+        public void WriteToFileTest()
+        {
+            var changedItems = new List<ILoggable>();
+
+            var customer = new CustomerDetails(1)
+            {
+                EmailAdrress = "bobosmard@xvideos.com",
+                FirstName = "Bobo ",
+                LastName = "Smrad",
+                AddressList = null
+            };
+            changedItems.Add(customer);
+
+            var product = new Product(2)
+            {
+                ProductName = "Lightsaber",
+                ProductDescription = "An elegant weapon for a more civilized age...",
+                CurrentPrice = 66
+            };
+            changedItems.Add(product);
+
+            LogginService.WriteToFile(changedItems);
+        }
+    }
+}
