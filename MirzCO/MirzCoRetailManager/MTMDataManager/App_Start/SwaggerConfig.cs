2059f7076a8d322ff62de9fe96067dc63bab7dec
MirzaSisic <mirzasisic57@gmail.com>
2019-12-29 12:42:26 +0100

    Configuring Swagger

--- MirzCO/MirzCoRetailManager/MTMDataManager/App_Start/SwaggerConfig.cs
+++ MirzCO/MirzCoRetailManager/MTMDataManager/App_Start/SwaggerConfig.cs
@@ -32,11 +32,11 @@ namespace MTMDataManager
                         // hold additional metadata for an API. Version and title are required but you can also provide
                         // additional fields by chaining methods off SingleApiVersion.
                         //
-                        c.SingleApiVersion("v1", "MTMDataManager");
+                        c.SingleApiVersion("v1", "MirzCo Retail Manager API");
 
                         // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                         //
-                        //c.PrettyPrint();
+                        c.PrettyPrint();
 
                         // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
                         // In this case, you must provide a lambda that tells Swashbuckle which actions should be
