560e8e73d6c62eef02b21e0979668aa4e68916e1
MirzaSisic <mirzasisic57@gmail.com>
2020-04-17 15:36:25 +0200

    ImplementingPageObjectModel

---
+++ tau/Royale/Pages/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Royale.Pages
+{
+    public class HeaderNav
+    {
+
+    }
+
+    public class HeaderNavMap
+    {
+    //     public IWebElement CardTabLink
+    }
+}
