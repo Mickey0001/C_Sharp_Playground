f14665c7bc5d4c6b843700720a8db06dc87dc46c
MirzaSisic <mirzasisic57@gmail.com>
2020-04-17 15:51:55 +0200

    HeaderNavClass

---
+++ tau/Royale/Pages/HeaderNav.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Royale.Pages
+{
+    public class HeaderNav
+    {
+        public readonly HeaderNavMap map;
+
+        public HeaderNav(IWebDriver driver)
+        {
+            map = new HeaderNavMap(driver);
+        }
+        public void GotoCardsPage()
+        {
+            map.CardsTabLink.Click();
+        }
+    }
+
+    public class HeaderNavMap
+    {  
+        IWebDriver _driver;
+
+        public HeaderNavMap(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+          public IWebElement CardsTabLink =>  _driver.FindElement(By.CssSelector("a[href='/cards']"));
+    }
+}
