c1cd0a9085233734b1fec17cceacb60d5a58277b
MirzaSisic <mirzasisic57@gmail.com>
2020-05-27 14:57:12 +0200

    SmokeTestSuite

---
+++ PortfolioTests/Tests/SmokeTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void VerifyHeaderText()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://mirzasisic.com/");
+            IWebElement HeaderText = driver.FindElement(By.TagName("h1"));
+            HeaderText.Equals("Mirza Šišić");
+            driver.Close();
+        }
+    }
+}
